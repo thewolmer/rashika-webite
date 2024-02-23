176b50480a1463f72cef577812fdb6bc7459ae74
Wolmer <contact@wolmer.me>
2024-02-23 12:58:47 +0530

    build: allow remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  // async redirects() {
+  //   return [{ source: '/collection/', destination: '/', permanent: false }];
+  // },
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false, net: false, tls: false };
+  //   config.externals.push('pino-pretty', 'lokijs', 'encoding');
+  //   return config;
+  // },
+};
 
 export default nextConfig;
